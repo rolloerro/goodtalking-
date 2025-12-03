3ee297454d86bed034b3e98846f77ffe6d2642cc
Vladimir Kopylov <pharmdoktor1972@icloud.com>
2025-12-03 22:11:34 +0300

    Initial Commit

---
+++ goodtalking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  goodtalking
+//
+//  Created by Vladimir Kopylov on 03/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ goodtalking/goodtalkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  goodtalkingApp.swift
+//  goodtalking
+//
+//  Created by Vladimir Kopylov on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct goodtalkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
